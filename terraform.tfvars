f70690308ea040219f0577e53f7fd3a0b03c01f7
pradeeparkumar <pradeep.ar.kumar@gmail.com>
2022-03-28 20:56:58 +0530

    branch

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+vpc_cidr_block = "10.0.0.0/16"
+
+public_subnet_block = "10.0.10.0/24"
+
+private_subnet_block = "10.0.20.0/24"
--- variable.tf
+++ variable.tf
@@ -1,3 +1,14 @@
 variable "vpc_cidr_block" {
-  description = "vpc cidr block"
-  }
+  description = "vpc_cidr_block"
+
+}
+
+variable "public_subnet_block" {
+    description = "public_subnet_block"
+  
+}
+
+variable "private_subnet_block" {
+  description = "private_subnet_block"
+  
+}
