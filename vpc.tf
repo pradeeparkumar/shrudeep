1a0649de2448f1b63dfe9d37624a7842f9e02474
pradeeparkumar <pradeep.ar.kumar@gmail.com>
2022-03-28 20:58:31 +0530

    commit

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,26 @@
 resource "aws_vpc" "veeru_vpc" {
     cidr_block = var.vpc_cidr_block
-    instance_tenancy = "default"
+    #instance_tenancy = "default"
+   
+}
+
+resource "aws_subnet" "public_subnet_block" {
+    vpc_id =  aws_vpc.veeru_vpc.id
+    cidr_block = var.public_subnet_block
+    availability_zone = "us-east-1a"
 
     tags = {
-      "pradeep" = "veeramruth"
+      "subnetpub" = "publicsubnet"
+    }
+}
+
+resource "aws_subnet" "private_subnet_block" {
+    vpc_id = aws_vpc.veeru_vpc.id
+    cidr_block = var.private_subnet_block
+    availability_zone = "us-east-1b"
+
+    tags = {
+      "subnetpri" = "privatesubnet"
     }
   
 }
